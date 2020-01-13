5b0489670d601b99c49ca0ace5ad88db2f43de6b
anna-postawka <anna.postawka@gmail.com>
2020-01-13 12:28:18 +0100

    added some styling

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "./Search";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Search />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Search.css";
+
+class Search extends React.Component {
+  render() {
+    return <h1 className="heading">Film Search</h1>;
+  }
+}
+
+export default Search;
